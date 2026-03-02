ca7e250a10c9ab3e14e5eebd3cfb4604e89dfe72
nan21psp <pyaesone35psp@gmail.com>
2026-03-02 12:33:00 +0630

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1500))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1003859091510))
+LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "1318826936"))
