72678ca97ba53889bfc70e10f7a53d71e8b9d978
nan21psp <pyaesone35psp@gmail.com>
2026-03-02 12:02:05 +0630

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/maythu-shar-music/svm",
+    "https://github.com/nan21psp/mm-music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
